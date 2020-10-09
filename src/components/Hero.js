e6585ddc3fd3cc09d0f2110f147db990c7e335f7
Hank Liu <hanke.liu@gmail.com>
2020-10-08 20:24:52 -0400

    format update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,7 +17,26 @@ const query = graphql`
   }
 `
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {file: {childImageSharp: {fluid}}} = useStaticQuery(query);
+  // console.log(data)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <h1>I'm Henry</h1>
+            <h4>freeland web developer</h4>
+              <Link to="/contact" className='btn'>
+                contact me
+              </Link>
+              <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+
+  )
 }
 
 export default Hero

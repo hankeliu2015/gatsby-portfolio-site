22e6e08fa904824d1441736f035c443aa0ffa549
Hank Liu <hanke.liu@gmail.com>
2020-10-09 22:22:48 -0400

    render data in blog component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -15,7 +15,6 @@ export const Blogs = ({blogs, title, showLink}) => {
       </div>
       {showLink && (<Link to="/blog" className="btn center-btn">blog</Link>)}
     </section>
-
   )
 }
 export default Blogs

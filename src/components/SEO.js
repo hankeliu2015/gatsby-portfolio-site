da79a39d21e88bcccbf3c5274ca09b6586b3af40
Hank Liu <hanke.liu@gmail.com>
2020-10-12 16:01:48 -0400

    add image, siteTitle and siteDesc metaData to header

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,11 +3,33 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 const SEO = ({title, description}) => {
+  const { allSite: {nodes: siteMetadata } } = useStaticQuery(query);
+
+  const { siteDesc, siteTitle, siteUrl, image, twitterUsername} = siteMetadata[0].siteMetadata
+
   return(
-    <Helmet htmlAttributes={{lang: "en"}} title={title}>
-      <meta name="description" content={description}></meta>
+    <Helmet htmlAttributes={{lang: "en"}} title={`${title} | siteTitle`}>
+      <meta name="description" content={description || siteDesc}></meta>
+      <meta name="image" content={image}></meta>
     </Helmet>
   )
 }
+
+const query = graphql`
+  {
+    allSite {
+      nodes {
+        siteMetadata {
+          author
+          siteDesc: description
+          image
+          siteUrl
+          siteTitle: title
+          twitterUsername
+        }
+      }
+    }
+  }
+`
 
 export default SEO
